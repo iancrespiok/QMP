e5c097a5f1572be23a6887567d405eacfb224be7
Ian Crespi <crespi.ian@gmail.com>
2021-04-21 00:34:15 -0300

    agrego test TODO

---
+++ src/test/java/domain/atuendos/prendatest.java
@@ -0,0 +1,11 @@
+package domain.atuendos;
+import com.sun.xml.internal.bind.v2.TODO;
+import org.junit.jupiter.api.Test;
+
+
+public class prendatest {
+    @Test
+    public void laParteDeAbajoDeAtuendoFormalEsUnPantalonDeVestir(){
+        //TODO
+    }
+}
